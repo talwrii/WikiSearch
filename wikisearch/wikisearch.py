22e8d993ec057939c6233ff9bb342dd400d9326b
Tal Wrii <talwrii@gmail.com>
2018-01-12 02:29:41 +0000

    Add entry point; make pip-installable

--- wikisearch/wikisearch.py
+++ wikisearch/wikisearch.py
@@ -1,38 +1,42 @@
-import bs4, requests, re, collections
-import sys, thread, time
-import string
+# make code as python 3 compatible as possible
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-def wiki_search(search_term):
-    #print "Search Term: " + search_term
+import bs4, requests, re, collections
+import sys, time
+import string
+import argparse
+
+def wiki_search(search_term, show_toc):
+    #print("Search Term: " + search_term)
     
     # Get the wiki page and turn it into Beatiful Soup
     wiki_page = requests.get("http://en.wikipedia.org/wiki/" + search_term)
-    wiki_soup = bs4.BeautifulSoup(wiki_page.content)
+    wiki_soup = bs4.BeautifulSoup(wiki_page.content.decode('utf8'), "lxml")
     summary_text = get_summary_text(wiki_soup)
     
     # Get the title of the wiki page
     title = wiki_soup.find('h1', class_='firstHeading').text
     
-    # Print the title and summary text
-    print "Title: " + title + '\n'
-    print summary_text
+    # Print(the title and summary text
+    print("Title: " + title + '\n')
+    print(summary_text)
     
     # Wait for the user to press Enter
-    raw_input("-----Press Enter To Continue-----")
-    print '\n'
-    
-    # Get the Table of Contents and print it
-    toc = wiki_soup.find('div', class_='toc')
-    
-    toc_dict = {}
-    toc_list = []
-    create_toc(toc, toc_dict, toc_list)
-    print_toc(toc_list)
+    if show_toc:
+        print('\n')
+
+        # Get the Table of Contents and print it
+        toc = wiki_soup.find('div', class_='toc')
+
+        toc_dict = {}
+        toc_list = []
+        create_toc(toc, toc_dict, toc_list)
+        print_toc(toc_list)
     
 def print_toc(toc_list):
     for (key, value) in toc_list:
         num_tabs = len(re.sub(r'[0-9]', '', key))
-        print ('  ' * num_tabs) + key + ' - ' + value
+        print(('  ' * num_tabs) + key + ' - ' + value)
 
 def create_toc(toc_html, toc_dict, toc_list):
     temp_list = []
@@ -41,7 +45,7 @@ def create_toc(toc_html, toc_dict, toc_list):
     for toc_item in toc_html.find_all('li'):
         # Print out the text for each link in each item
         for link in toc_item.find_all('a'):
-            link_text = link.text.encode('utf-8')
+            link_text = link.text
             key = link_text.split(' ')[0]
             value = ' '.join(link_text.split(' ')[1:])
             toc_dict[key] = value
@@ -68,10 +72,22 @@ def get_summary_text(wiki_soup):
         if element.get('id') == 'toc':
             break
     return ''.join(summary_text)
-            
 
-def main(argv):
-    wiki_search(argv[0])
+
+def build_parser():
+    PARSER = argparse.ArgumentParser(description='')
+    PARSER.add_argument('search', type=str)
+    PARSER.add_argument('--no-toc', action='store_false', default=True, dest='toc')
+    return PARSER
+
+def main(argv=None):
+    # setuptools entry point
+    if argv is None:
+        argv = sys.argv[1:]
+
+    args = build_parser().parse_args()
+
+    wiki_search(args.search, args.toc)
 
 if __name__ == '__main__':
     #Pass command line args to main
