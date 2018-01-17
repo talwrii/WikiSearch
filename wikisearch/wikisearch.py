0b7cc738b77fbab1d9e42682a5af788bc1cfc5b0
Tal Wrii <talwrii@gmail.com>
2018-01-17 09:09:05 +0000

    whitespace

--- wikisearch/wikisearch.py
+++ wikisearch/wikisearch.py
@@ -8,19 +8,20 @@ import argparse
 
 def wiki_search(search_term, show_toc):
     #print("Search Term: " + search_term)
-    
+
+
     # Get the wiki page and turn it into Beatiful Soup
     wiki_page = requests.get("http://en.wikipedia.org/wiki/" + search_term)
     wiki_soup = bs4.BeautifulSoup(wiki_page.content.decode('utf8'), "lxml")
     summary_text = get_summary_text(wiki_soup)
-    
+
     # Get the title of the wiki page
     title = wiki_soup.find('h1', class_='firstHeading').text
-    
+
     # Print(the title and summary text
     print("Title: " + title + '\n')
     print(summary_text)
-    
+
     # Wait for the user to press Enter
     if show_toc:
         print('\n')
@@ -32,7 +33,7 @@ def wiki_search(search_term, show_toc):
         toc_list = []
         create_toc(toc, toc_dict, toc_list)
         print_toc(toc_list)
-    
+
 def print_toc(toc_list):
     for (key, value) in toc_list:
         num_tabs = len(re.sub(r'[0-9]', '', key))
@@ -40,7 +41,7 @@ def print_toc(toc_list):
 
 def create_toc(toc_html, toc_dict, toc_list):
     temp_list = []
-    
+
     # Get all lists from the Beautiful Soup
     for toc_item in toc_html.find_all('li'):
         # Print out the text for each link in each item
@@ -50,11 +51,11 @@ def create_toc(toc_html, toc_dict, toc_list):
             value = ' '.join(link_text.split(' ')[1:])
             toc_dict[key] = value
             temp_list.append((key, value))
-    
+
     for item in temp_list:
         if item not in toc_list:
             toc_list.append(item)
-            
+
     last_num = toc_list[-1:][0][0].split('.')[0]
     toc_dict[last_num] = "Search Again"
     # Passing (last_num + 1, "Search Again")
@@ -63,7 +64,7 @@ def create_toc(toc_html, toc_dict, toc_list):
 
 def get_summary_text(wiki_soup):
     # Get the location of where the summary is at
-    summary_soup = wiki_soup.find('div', id='mw-content-text') 
+    summary_soup = wiki_soup.find('div', id='mw-content-text')
     summary_text = []
     for element in summary_soup.find_all(True):
         # print element
